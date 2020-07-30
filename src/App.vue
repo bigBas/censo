90a851b3cecb009550e870a61e6755f9c3ac18ec
bigBas <54853679+bigBas@users.noreply.github.com>
2020-07-30 00:40:40 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header />
+      <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import Vue from 'vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'app',
+  components: {
+        Header,
+        Footer
+    },
+}
+</script>
+
+<style scoped>
+
+#app {
+  
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+    <header id="iniLetter">
+        <div class="container-fluid header-h">
+            <div class="col-4 offset-1 d-inline-flex p-0 align-items-center">
+                <a href="https://www.unam.mx/"
+                    ><img
+                        src="../img/Logo-UNAM.png"
+                        class="img-fluid"
+                        alt="Logo UNAM"
+                /></a>
+            </div>
+            <div
+                class="col-4  offset-2 d-inline-flex justify-content-end p-0 align-items-center"
+            >
+                <a href="http://www.acatlan.unam.mx/">
+                    <img
+                        src="../img/logo_fesa.png"
+                        class="img-fluid"
+                        alt="Logo Fes Acatlán"
+                    />
+                </a>
+            </div>
+        </div>
+
+        <div
+            class="container-fluid header-f d-flex justify-content-end align-items-center"
+        >
+            <!-- v-if="!['login'].includes($route.name)" va arriba, pero después 7u7-->
+            <!-- <div
+                class="mr-3"
+            >
+                <button
+                    class="btn btn-danger m-0 px-3 py-2"
+                >
+                Cerrar Sesión
+                </button>
+            </div> -->
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style>
+.header-h {
+    /* background: #1b3d70; */
+    background: #1b3d70;
+    padding: 1rem 0;
+}
+
+.contenedor-header {
+    padding: 1rem 0;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+}
+
+.contenedor-img {
+    width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.contenedor-img img {
+    max-width: 100%;
+}
+
+.header-f {
+    padding: 0.5rem 0;
+    background-color: #bb8800;
+}
+#info {
+    color: white;
+}
+.sesion {
+    margin: 0.5rem;
+    line-height: 0.5rem !important;
+}
+</style>
